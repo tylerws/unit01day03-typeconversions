f551b0821a0e9ed665ac77de7aecf56992d44895
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-01-12 12:22:24 -0600

    completed quadratic

--- Quadratic.java
+++ Quadratic.java
@@ -1,6 +1,14 @@
 public class Quadratic {
     public static void main(String[] args) {
-        int 
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        double bsq = Math.pow(b, 2);
+        double result1 =  (-b + Math.sqrt(bsq - (4 * a * c))) / (2 * a);
+        double result2 =  (-b - Math.sqrt(bsq - (4 * a * c))) / (2 * a);
+        System.out.println(result1 + "," + result2);
+        
+
 
 
     }
